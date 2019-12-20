e8c59a3c5b3b3ac1fb354b8d4965148404ff84c8
sgmcarvalho <58332035+sgmcarvalho@users.noreply.github.com>
2019-12-20 15:14:13 -0300

    Create Tabelas_resultados_rnaseq-ref.sh

---
+++ Tabelas_resultados_rnaseq-ref.sh
@@ -0,0 +1,25 @@
+#Resultados para o script rnaseq-ref.sh
+
+|                |N.seq submetidas|processadas(atropos)|processadas(prinseq)|alinhadas|alinhadas(flag 147)|
+|SAMPLEA1_R1     |     25000      |       24505        |       24187        |  Total  |       Total       |
+|         R2     |     25000      |       24505        |       24187        |  48725  |       9724        |
+|SAMPLEA2_R2     |     25000      |       24490        |       24163        |  Total  |       Total       |
+|         R1     |     25000      |       24490        |       24163        |  48675  |       9677        |
+|SAMPLEB1_R1     |     25000      |       24454        |       24163        |  Total  |       Total       |
+|         R2     |     25000      |       24454        |       24163        |  48629  |       12075       |
+|SAMPLEB2_R1     |     25000      |       24525        |       24213        |  Total  |       Total       |
+|         R2     |     25000      |       24525        |       24213        |  48741  |       12124       |
+
+
+
+|Gene ID | Gene |   Nome NCBI   | Abundancia A | Abundancia B |            Locus             | Amostra |  Log2FC  | E-value |
+|MSTRG.1 | AP1  | NM_105581.3   |    0.50      |     0.10     |NC_003070.9::25982293-25986349| SAMPLEA | -2.18447 |  5e-05  |
+|        |      |               |              |              |                              | SAMPLEB | -56.7091 |         |
+|MSTRG.3 | STK  | NM_001340645.1|    0.10      |     0.40     |NC_003075.7:6234670-6241281   | SAMPLEA |  56.1361 |  5e-05  |
+|        |      |               |              |              |                              | SAMPLEB |  2.19709 |         |
+|MSTRG.4 | AG   | NM_001341309.1|    0.05      |     0.05     |NC_003075.7:10382855-10388539 | SAMPLEA | 0.284579 |  5e-05  |
+|        |      |               |              |              |                              | SAMPLEB |  4.54284 |         |
+|MSTRG.5 | PI   | NM_122031.4   |    0.05      |     0.05     |NC_003076.8:6828903-6831515   | SAMPLEA | 0.234636 |  5e-05  |
+|        |      |               |              |              |                              | SAMPLEB |  3.74928 |         |
+|MSTRG.6 | LFY  | NM_125579.1   |    0.25      |     0.05     |NC_003076.8:24844247-24846989 | SAMPLEA |0.0570235 | 0.00055 |
+|        |      |               |              |              |                              | SAMPLEB |  1.99142 |         |
